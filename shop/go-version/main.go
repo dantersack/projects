587d63b72d9ba5cec25229d1ced8cfb18a4a5196
Dante Sack <sack_dante@hotmail.com>
2024-05-06 16:38:02 -0300

    add more logic to func handlers

--- shop/go-version/main.go
+++ shop/go-version/main.go
@@ -3,24 +3,67 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 )
+
+type Product struct {
+	Id          int
+	Description string
+	Price       float64
+}
+
+var id int = 0
+
+var products = []Product{}
+
+func seedProducts(products *[]Product) {
+	for i := 0; i < 3; i++ {
+		id++
+		*products = append(*products, Product{Id: id, Description: fmt.Sprintf("Test_%d", id), Price: 99.99})
+	}
+}
+
+func findProduct(id int, products []Product) (Product, error) {
+	for _, val := range products {
+		if val.Id == id {
+			return val, nil
+		}
+	}
+	return Product{}, fmt.Errorf("Product with id=%d not found", id)
+}
 
 func main() {
 	fmt.Println("Shop REST API using Go 1.22")
 
+	seedProducts(&products)
+
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /products", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "return all products\n")
+		fmt.Fprintf(w, "Products: %v", products)
 	})
 
 	mux.HandleFunc("GET /products/{id}", func(w http.ResponseWriter, r *http.Request) {
-		id := r.PathValue("id")
-		fmt.Fprintf(w, "return product with id: %s\n", id)
+		numericId, castError := strconv.Atoi(r.PathValue("id"))
+		if castError != nil {
+			panic(castError)
+		}
+		searchedProduct, errorWhileSearching := findProduct(numericId, products)
+		if errorWhileSearching != nil {
+			fmt.Fprintf(w, "Error! Product not found!")
+		} else {
+			fmt.Fprintf(w, "Product found: %v", searchedProduct)
+		}
 	})
 
 	mux.HandleFunc("POST /products", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "create a new product\n")
+		p := Product{
+			Id:          id + 1,
+			Description: "New product",
+			Price:       1200,
+		}
+		products = append(products, p)
+		fmt.Fprintf(w, "Product created: %v", p)
 	})
 
 	if err := http.ListenAndServe("localhost:8080", mux); err != nil {
