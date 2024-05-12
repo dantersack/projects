84684fb09fbcb5fa76794755d12a44b9c70e703a
Dante Sack <sack_dante@hotmail.com>
2024-05-12 17:56:18 -0300

    coding challenges: arrays and hashing:: two sum - improve algorithm

--- _coding-challenges/go/arrays-and-hashing/two-sum/main.go
+++ _coding-challenges/go/arrays-and-hashing/two-sum/main.go
@@ -1,25 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Println(twoSum([]int{7, 2, 11, 15, 4, 6, 1}, 9))
 }
 
 func twoSum(nums []int, target int) []int {
-	var idx1 int
-	var idx2 int
-
 	for i, vi := range nums {
 		for j, vj := range nums {
 			if i != j && vi+vj == target {
-				idx1 = i
-				idx2 = j
-				break
+				return []int{i, j}
 			}
 		}
-
 	}
 
-	return []int{idx1, idx2}
+	return []int{}
 }
